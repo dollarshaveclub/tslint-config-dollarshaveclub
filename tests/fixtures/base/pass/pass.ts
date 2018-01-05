b4076c30f66bdd350ca3803c9405c739c5b1de2d
Jeff Wainwright <yowainwright@gmail.com>
2018-01-04 19:05:06 -0800

    [update] updates file structure in tests

---
+++ tests/fixtures/base/pass/pass.ts
@@ -0,0 +1,23 @@
+
+const noSemiColons: String = 'please'
+console.log(noSemiColons)
+
+const trailingCommasPlease: Number[] = [
+  1,
+  2,
+  3,
+]
+
+console.log(trailingCommasPlease)
+
+{
+  async function foo () {
+    const myArray: Number[] = [1, 2, 3]
+    for (let somePromise of myArray) {
+      let nonPromise: Number = await somePromise
+      console.log(nonPromise)
+    }
+  }
+
+  foo()
+}
