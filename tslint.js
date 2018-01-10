8d9e6ff236c485408885dbe72b8d85fc5add9f10
Jeff Wainwright <yowainwright@gmail.com>
2018-01-10 15:16:58 -0800

    [update] news rules + v1

--- tslint.js
+++ tslint.js
@@ -1,12 +1,11 @@
-"use strict"
+"use strict";
 
 module.exports = {
-  extends: ['tslint-config-standard'],
+  extends: ["tslint-config-standard"],
   rules: {
     "await-promise": false, // awaiting fix in next version https://github.com/palantir/tslint/issues/3381
-    "jsx-no-lambda": false,
     "no-empty": false,
-    "trailing-comma": [true, { "multiline": "always", "singleline": "never" }],
+    "no-floating-promises": false,
+    "trailing-comma": [true, { multiline: "always", singleline: "never" }],
   },
-}
-
+};
