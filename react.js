d0888175550270296b3279ffe507662bcb637f5b
bensonnn <dan.s.benson@gmail.com>
2019-04-18 13:32:34 -0700

    prevent importing from /src in dsc modules

--- react.js
+++ react.js
@@ -5,5 +5,10 @@ module.exports = {
   rules: {
     'jsx-no-lambda': false,
     'jsx-no-multiline-js': false,
+    'import-blacklist': [
+      true,
+      ['^@dollarshaveclub/.*/src/.*'], // prevent importing from src
+
+    ],
   },
 }
